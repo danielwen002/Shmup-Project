a1558622744f757bd1d2ab950158f909a98e6e1f
2020vbo <victorbo2020@gmail.com>
2017-05-18 13:25:28 -0400

    Add files via upload

--- GameBoard.java
+++ GameBoard.java
@@ -7,8 +7,7 @@ public class GameBoard extends JPanel {
 
    private static final Color BACKGROUND = new Color(204, 204, 204);
 
-   private int mouseX;
-   private int mouseY;
+   private Point mouse;
    private Graphics myBuffer;
    private BufferedImage myImage;
    private Player player;
@@ -21,28 +20,43 @@ public class GameBoard extends JPanel {
       Cursor blankCursor = Toolkit.getDefaultToolkit().createCustomCursor(cursorImg, new Point(0, 0), "blank cursor");
       setCursor(blankCursor);
       
+      myBuffer.setColor(BACKGROUND);  
+      myBuffer.fillRect(0, 0, 350, 500);
+         
       player = new Player();
       player.draw(myBuffer);
-   
-      addMouseMotionListener(new MouseListener());
       
-      Timer runGame = new Timer(17, new GameListener());
+      Timer runGame = new Timer(5, new GameListener());
       runGame.start();
    }
   
    private class GameListener implements ActionListener {
       public void actionPerformed(ActionEvent e) {
-         player.setX(mouseX - 25);
-         player.setY(mouseY - 25);
+         myBuffer.setColor(BACKGROUND);  
+         myBuffer.fillRect(0, 0, 350, 500);
+         
+         mouse = getMousePosition();
+         
+         if(mouse != null) {
+            player.setX(mouse.x);
+            player.setY(mouse.y);
+            
+            if(player.getX() < 25) {
+               player.setX(25);
+            }
+            if(player.getX() > 325) {
+               player.setX(325);
+            }
+            
+            System.out.println("Mouse X: " + mouse.x + "\tMouse Y: " + mouse.y);
+            System.out.println("Player X: " + player.getX() + "\tPlayer Y: " + player.getY());
+         }
          player.draw(myBuffer);   
+         repaint();
       }
    }
-   private class MouseListener extends MouseMotionAdapter {
-      public void mouseMoved(MouseEvent e) {
-         mouseX = e.getX();
-         mouseY = e.getY();
-         
-         System.out.println("X: " + mouseX + "\tY: " + mouseY);
-      }
+   public void paintComponent(Graphics g)
+   {
+      g.drawImage(myImage, 0, 0, getWidth(), getHeight(), null);
    }
 }
--- Player.java
+++ Player.java
@@ -5,9 +5,10 @@ public class Player {
    private int myX;
    private int myY;
    private int myCurrentHealth;
-   private ImageIcon sprite = new ImageIcon("Sprites/Player.png");
+   private ImageIcon sprite;
    
    public Player() {
+      sprite = new ImageIcon("Sprites/Player.png");
       myCurrentHealth = 150;
       myX = 175;
       myY = 400;
@@ -18,20 +19,23 @@ public class Player {
    public int getY() {
       return myY;
    }
-   public void setX(int x) {
-      myX = x;
-   }
-   public void setY(int y) {
-      myY = y;
-   }
    public int getCurrentHealth() {
       return myCurrentHealth;
    }
    public Image getSprite() {
       return sprite.getImage();
    }
+   public void setX(int x) {
+      myX = x;
+   }
+   public void setY(int y) {
+      myY = y;
+   }
+   public void setCurrentHealth(int hp) {
+      myCurrentHealth = hp;
+   }
    public void draw(Graphics myBuffer) {
-      myBuffer.drawImage(getSprite(), getX(), getY(), 50, 50, null);
+      myBuffer.drawImage(getSprite(), getX() - 25, getY() - 25, 50, 50, null);
    }
    public void attack() {
    
